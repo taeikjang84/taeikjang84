b549f05d36e8f93217c5d25bb539210cdfb1f616
taeikjang84 <83894061+taeikjang84@users.noreply.github.com>
2021-05-14 20:38:09 +0900

    Update server_FLOW.py

--- server_FLOW.py
+++ server_FLOW.py
@@ -28,10 +28,10 @@ while True:
         m5_FLOW_1_2 = m5_FLOW_1[-2]
         m5_FLOW_1_1 = m5_FLOW_1[-1]
         op_mode_FLOW = True
-        time.sleep(1)
+
   
     price_FLOW = pyupbit.get_current_price("KRW-FLOW")
-    time.sleep(1)
+ 
    
 
    
@@ -63,6 +63,6 @@ while True:
         # time.sleep(1)
      
     print(f"time: {now} hold: {hold_FLOW} op: {op_mode_FLOW}")
-    time.sleep(1)
+ 
    
 
