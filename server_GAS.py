4196cf478ba886674aaa12c66cccf893e925028a
taeikjang84 <83894061+taeikjang84@users.noreply.github.com>
2021-05-14 20:38:26 +0900

    Update server_GAS.py

--- server_GAS.py
+++ server_GAS.py
@@ -28,10 +28,10 @@ while True:
         m5_GAS_1_2 = m5_GAS_1[-2]
         m5_GAS_1_1 = m5_GAS_1[-1]
         op_mode_GAS = True
-        time.sleep(1)
+
   
     price_GAS = pyupbit.get_current_price("KRW-GAS")
-    time.sleep(1)
+
    
 
    
@@ -63,6 +63,6 @@ while True:
         # time.sleep(1)
      
     print(f"time: {now} hold: {hold_GAS} op: {op_mode_GAS}")
-    time.sleep(1)
+
    
 
