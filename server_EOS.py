8061eb535497bccf4f6d9ef1d1faa227e2c6dcda
taeikjang84 <83894061+taeikjang84@users.noreply.github.com>
2021-05-14 20:37:15 +0900

    Update server_EOS.py

--- server_EOS.py
+++ server_EOS.py
@@ -28,10 +28,10 @@ while True:
         m5_EOS_1_2 = m5_EOS_1[-2]
         m5_EOS_1_1 = m5_EOS_1[-1]
         op_mode_EOS = True
-        time.sleep(1)
+
   
     price_EOS = pyupbit.get_current_price("KRW-EOS")
-    time.sleep(1)
+
    
 
    
@@ -63,6 +63,6 @@ while True:
         # time.sleep(1)
      
     print(f"time: {now} hold: {hold_EOS} op: {op_mode_EOS}")
-    time.sleep(1)
+
    
 
